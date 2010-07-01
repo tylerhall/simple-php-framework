2d06b071d5a2065cc48d856954114e006b2464dc
Tyler Hall <thall@imac.local>
2010-07-01 14:01:48 -0500

    Adding DBLoop class, which is a memory-light alternative to DBObject::glob(). Credit goes to Jason Tan (@jt2k) for the idea.

---
+++ includes/class.dbloop.php
@@ -0,0 +1,73 @@
+<?PHP
+    class DBLoop implements Iterator
+    {
+        private $position;
+        private $className;
+        private $extraColumns;
+        private $result;
+
+        public function __construct($class_name, $sql = null, $extra_columns = array())
+        {
+            $this->position     = 0;
+            $this->className    = $class_name;
+            $this->extraColumns = $extra_columns;
+
+            // Make sure the class exists before we instantiate it...
+            if(!class_exists($class_name))
+                return;
+
+            $tmp_obj = new $class_name;
+
+            // Also, it needs to be a subclass of DBObject...
+            if(!is_subclass_of($tmp_obj, 'DBObject'))
+                return;
+
+            if(is_null($sql))
+                $sql = "SELECT * FROM `{$tmp_obj->tableName}`";
+
+            $db = Database::getDatabase();
+            $this->result = $db->query($sql);
+        }
+
+        function rewind()
+        {
+            $this->position = 0;
+        }
+
+        function current()
+        {
+            mysql_data_seek($this->result, $this->position);
+            $row = mysql_fetch_array($this->result, MYSQL_ASSOC);
+            if($row === false)
+                return false;
+
+            $o = new $this->className;
+            $o->load($row);
+
+            foreach($this->extraColumns as $c)
+            {
+                $o->addColumn($c);
+                $o->$c = isset($row[$c]) ? $row[$c] : null;
+            }
+
+            return $o;
+        }
+
+        function key()
+        {
+            return $this->position;
+        }
+
+        function next()
+        {
+            $this->position++;
+        }
+
+        function valid()
+        {
+            if($this->position < mysql_num_rows($this->result))
+                return mysql_data_seek($this->result, $this->position);
+            else
+                return false;
+        }
+    }
