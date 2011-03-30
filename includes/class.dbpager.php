bf79625af72d954da077901feccf1b4d444bd7a1
Tyler Hall <thall@mercuryintermedia.com>
2011-03-30 10:30:56 -0500

    Modifying code style of new dbpager class courtesey of klanoma/simple-php-framework

--- includes/class.dbpager.php
+++ includes/class.dbpager.php
@@ -1,24 +1,27 @@
-<?php
-class DBPager extends Pager {
-  private $itemClass;
-  private $countSql;
-  private $pageSql;
-  
-  public function __construct($itemClass, $countSql, $pageSql, $page, $per_page) {
-    $this->itemClass = $itemClass;
-    $this->countSql = $countSql;
-    $this->pageSql = $pageSql;
+<?PHP
+    class DBPager extends Pager
+    {
+        private $itemClass;
+        private $countSql;
+        private $pageSql;
 
-    $db = Database::getDatabase();
-    $num_records = intval($db->getValue($countSql));
-    
-    parent::__construct($page, $per_page, $num_records);
-  }
-  
-  public function calculate() {
-    parent::calculate();
-    //load records .. see $this->firstRecord, $this->perPage
-    $limitSql = sprintf(" LIMIT %s,%s", $this->firstRecord, $this->perPage);
-    $this->records = array_values(DBObject::glob($this->itemClass, $this->pageSql . $limitSql));
-  }
-}
+        public function __construct($itemClass, $countSql, $pageSql, $page, $per_page)
+        {
+            $this->itemClass = $itemClass;
+            $this->countSql  = $countSql;
+            $this->pageSql   = $pageSql;
+
+            $db = Database::getDatabase();
+            $num_records = intval($db->getValue($countSql));
+
+            parent::__construct($page, $per_page, $num_records);
+        }
+
+        public function calculate()
+        {
+            parent::calculate();
+            // load records .. see $this->firstRecord, $this->perPage
+            $limitSql = sprintf(' LIMIT %s,%s', $this->firstRecord, $this->perPage);
+            $this->records = array_values(DBObject::glob($this->itemClass, $this->pageSql . $limitSql));
+        }
+    }
