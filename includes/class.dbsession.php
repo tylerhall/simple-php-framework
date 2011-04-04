87616bcd268ccfc0a391397860a477380a9255d0
Tyler Hall <tylerhall@gmail.com>
2011-04-04 13:33:43 -0700

    Edited includes/class.dbsession.php via GitHub

--- includes/class.dbsession.php
+++ includes/class.dbsession.php
@@ -9,7 +9,7 @@
 
         public static function open()
         {
-            $db = Database::getDatabase(true);
+            $db = Database::getDatabase();
             return $db->isWriteConnected();
         }
 
@@ -20,14 +20,14 @@
 
         public static function read($id)
         {
-            $db = Database::getDatabase(true);
+            $db = Database::getDatabase();
             $db->query('SELECT `data` FROM `sessions` WHERE `id` = :id:', array('id' => $id));
             return $db->hasRows() ? $db->getValue() : '';
         }
 
         public static function write($id, $data)
         {
-            $db = Database::getDatabase(true);
+            $db = Database::getDatabase();
             $db->query('DELETE FROM `sessions` WHERE `id` = :id:', array('id' => $id));
             $db->query('INSERT INTO `sessions` (`id`, `data`, `updated_on`) VALUES (:id:, :data:, :updated_on:)', array('id' => $id, 'data' => $data, 'updated_on' => time()));
             return ($db->affectedRows() == 1);
@@ -35,14 +35,14 @@
 
         public static function destroy($id)
         {
-            $db = Database::getDatabase(true);
+            $db = Database::getDatabase();
             $db->query('DELETE FROM `sessions` WHERE `id` = :id:', array('id' => $id));
             return ($db->affectedRows() == 1);
         }
 
         public static function gc($max)
         {
-            $db = Database::getDatabase(true);
+            $db = Database::getDatabase();
             $db->query('DELETE FROM `sessions` WHERE `updated_on` < :updated_on:', array('updated_on' => time() - $max));
             return true;
         }
