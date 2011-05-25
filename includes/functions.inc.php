3bddbbbd03706c1da82950e25c4b42c2051de892
Tyler Hall <tylerhall@gmail.com>
2011-05-25 08:27:16 -0700

    Removed a rogue echo statement from functions.inc.php

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -584,10 +584,9 @@
 
     function framework_autoload($class_name)
     {
-        $file = DOC_ROOT . '/includes/class.' . strtolower($class_name) . '.php';
-        echo $file;
-        if (file_exists($file))
-            require $file;
+        $filename = DOC_ROOT . '/includes/class.' . strtolower($class_name) . '.php';
+        if(file_exists($filename))
+            require $filename;
     }
 
     // Returns a file's mimetype based on its extension
