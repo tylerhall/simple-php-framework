1b436585fa378191f4b821e9b682ae85357983d5
Jhobanny Morillo Ordoñez <geomorillo@origenet.cl>
2014-04-03 18:25:28 -0400

    Basic language support in the login for spanish

---
+++ lang/es.php
@@ -0,0 +1,9 @@
+<?php
+
+/* SPANISH-ESPAÑOL */
+define("TITLE_PAGE", "Página de ejemplo");
+define('USER_NAME_LABEL', "Usuario:");
+define("PASSWORD_LABEL", "Contraseña:");
+define('LOGIN_ERROR', 'Lo sentimos, ud ha ingresado un nombre de usuario y contraseña incorrecta. Intente de nuevo');
+
+
---
+++ lang/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+// Here is a basic register for languages for not cluttering master.inc.php
+// include any language you want and create a php just enable ONLY 1 you cannot 
+// enable 2 or more at the same time
+
+/* * ******************************************************** */
+require DOC_ROOT . '/lang/es.php'; //Español - Spanish
+//require DOC_ROOT . '/lang/en.php'; //English Enable for support
+/************************************************************/
