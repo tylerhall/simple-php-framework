c70d0772be091a50f2bb5fa905dcbe6c418870f8
Tyler Hall <tylerhall@gmail.com>
2015-03-18 14:49:30 -0500

    Fetch system timezone function.

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -1,4 +1,18 @@
 <?PHP
+	function timezone_list()
+	{
+		$zones_array = array();
+		$timestamp = time();
+		foreach(timezone_identifiers_list() as $key => $zone)
+		{
+			date_default_timezone_set($zone);
+			$zones_array[$key]['zone'] = $zone;
+			$zones_array[$key]['diff_from_GMT'] = 'UTC/GMT ' . date('P', $timestamp);
+		}
+		date_default_timezone_set('UTC');
+		return $zones_array;
+	}
+	
     function set_option($key, $val)
     {
         $db = Database::getDatabase();
