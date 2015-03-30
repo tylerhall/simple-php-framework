67baa814db7abc55d50cda5a44659fcc29735ae7
Tyler Hall <tylerhall@gmail.com>
2015-03-30 14:28:21 -0500

    Re-adding User class that went missing.

--- includes/class.objects.php
+++ includes/class.objects.php
@@ -1,2 +1,10 @@
 <?PHP
     // Stick your DBOjbect subclasses in here (to help keep things tidy).
+
+    class User extends DBObject
+    {
+        public function __construct($id = null)
+        {
+            parent::__construct('users', array('nid', 'username', 'password', 'level'), $id);
+        }
+    }
